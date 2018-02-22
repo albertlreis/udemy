b589408cfa16e8d342d830c6b22073e64689f091
Albert Lima <albertlreis@gmail.com>
2018-02-22 12:16:58 -0300

    Criando Imagem a partir de uma existente - Certificado

--- exemplo-08.php
+++ exemplo-08.php
@@ -1,19 +1,30 @@
 <?php
 
-header("Content-Type: image/png");
+//header("Content-Type: image/png");
 
-$image = imagecreate(256, 256);
+//$image = imagecreate(256, 256);
+//
+//$black = imagecolorallocate($image, 0, 0, 0);
+//
+//$red = imagecolorallocate($image, 255, 0, 0);
+//
+//$green = imagecolorallocate($image, 0, 255, 0);
+//
+//$blue = imagecolorallocate($image, 0, 0, 255);
+//
+//imagestring($image, 5, 60, 120, "Curso PHP 7", $red);
 
-$black = imagecolorallocate($image, 0, 0, 0);
+$image = imagecreatefromjpeg("certificado.jpg");
 
-$red = imagecolorallocate($image, 255, 0, 0);
+$titleColor = imagecolorallocate($image, 0, 0, 0);
+$grey = imagecolorallocate($image, 100, 100, 100);
 
-$green = imagecolorallocate($image, 0, 255, 0);
+imagestring($image, 5, 450, 150, "CERTIFICADO", $titleColor);
+imagestring($image, 5, 440, 350, "ALBERT REIS", $titleColor);
+imagestring($image, 5, 440, 370, utf8_decode("Concluído em: ").DATE("d/m/Y"), $titleColor);
 
-$blue = imagecolorallocate($image, 0, 0, 255);
+header("Content-type: image/jpeg");
 
-imagestring($image, 5, 60, 120, "Curso PHP 7", $red);
-
-imagepng($image);
+imagejpeg($image, "Certificado-".date("Y-m-d").".jpeg", 10);
 
 imagedestroy();
