50e16f2b570f85b90af74db3a4f5f808bc092236
Albert Lima <albertlreis@gmail.com>
2018-02-23 13:00:10 -0300

    Utilizando reCaptcha

---
+++ seguranca/exemplo-04.php
@@ -0,0 +1,12 @@
+<script src='https://www.google.com/recaptcha/api.js'></script>
+
+
+<form action="cadastrar.php" method="post">
+
+    <input type="email" name="email">
+
+    <div class="g-recaptcha" data-sitekey="6Lc23TEUAAAAADtdd5AkyZw1gp9J_HkGxNY1bXtN"></div>
+
+    <button type="submit">Enviar</button>
+
+</form>
